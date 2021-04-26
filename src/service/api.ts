58bc73be23806d176ab4aa3233a11440ae0e188b
Crystian Lefundes <crystian@pop-os.localdomain>
2021-04-26 03:32:05 -0300

    update server

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.24:3333'
+    baseURL: 'https://my-json-server.typicode.com/CrysLef/plantmanager-NLW-5'
 })
 
 export default api;
